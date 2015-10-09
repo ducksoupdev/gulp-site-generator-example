82789f1eee2e30a9fd560aebfeee4729ee113e15
levym <ducksoupdev@gmail.com>
2015-10-09 22:18:20 +0100

    Implement static pages branch.

--- gulpfile.js
+++
@@ -1,8 +0,0 @@
-(function() {
-    "use strict";
-
-    var requireDir = require("require-dir");
-
-    // require all tasks in gulp/tasks, including sub-folders
-    requireDir("./tools/gulp/tasks", { recurse: true });
-})();
